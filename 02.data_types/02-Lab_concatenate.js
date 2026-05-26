b96fb642c520e1906991a154b52ea9abf794b99a
Maria Kalinina <marykalinina86449@gmail.com>
2026-05-26 12:37:04 +0300

    Delete 02.data_types/02-Lab_concatenate.js

--- 02.data_types/02-Lab_concatenate.js
+++
@@ -1,6 +0,0 @@
-function concatenateNames(name,surname, delimiter) {
-    console.log(`${name}${delimiter}${surname}`);
-}
-concatenateNames('John', 'Smith', '->');
-concatenateNames('Jan', 'White', '<->');
-concatenateNames('Linda', 'Terry', '=>');
