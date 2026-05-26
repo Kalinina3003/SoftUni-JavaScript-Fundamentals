2cafd756173af0071a1fd4af49b3be37e7148649
Maria Kalinina <marykalinina86449@gmail.com>
2026-05-26 12:37:36 +0300

    Add files via upload

---
+++ 02.data_types/02-Lab_concatenate.js
@@ -0,0 +1,9 @@
+// Task: Names Join
+// Description: Join two strings using a given delimiter and print the result.
+
+function concatenateNames(name,surname, delimiter) {
+    console.log(`${name}${delimiter}${surname}`);
+}
+concatenateNames('John', 'Smith', '->');
+concatenateNames('Jan', 'White', '<->');
+concatenateNames('Linda', 'Terry', '=>');
