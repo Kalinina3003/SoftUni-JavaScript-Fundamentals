7aef408a73db56f28619c7f97961f30e933d1707
Maria Kalinina <marykalinina86449@gmail.com>
2026-06-01 19:24:34 +0300

    Add files via upload

---
+++ 02.data_types/01-Ex_sum.js
@@ -0,0 +1,16 @@
+// Task: Sum Digits
+// Description: Find and print the sum of all digits in a given number.
+
+function sumDigits(num) {
+    let numAsString = String(num);
+    let sum = 0;
+
+    for (let i = 0; i < numAsString.length; i++) {
+        let currentNum = Number(numAsString[i]);
+        sum += currentNum;
+    }
+    console.log(sum);
+}
+sumDigits(245678);
+sumDigits(97561);
+sumDigits(543);
