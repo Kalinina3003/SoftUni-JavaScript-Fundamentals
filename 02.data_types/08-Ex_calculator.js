832111f83ee121734d4ef5892594d5fe033941bc
Maria Kalinina <marykalinina86449@gmail.com>
2026-06-02 13:19:48 +0300

    Add files via upload

---
+++ 02.data_types/08-Ex_calculator.js
@@ -0,0 +1,20 @@
+// Task: Calculator
+// Description: Perform the calculation based on the operator.
+
+function calculator(num1, operator, num2) {
+    let result = 0;
+
+    if (operator === '+') {
+        result = num1 + num2;
+    } else if (operator === '-') {
+        result = num1 - num2;
+    } else if (operator === '*') {
+        result = num1 * num2;
+    } else if (operator === '/') {
+        result = num1 / num2;
+    }
+
+    console.log(result.toFixed(2));
+}
+calculator(5, '+', 10);
+calculator(25.5, '-', 3);
