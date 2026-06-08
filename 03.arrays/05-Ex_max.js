278efb276e487a7273f9a5b09535cdb49efd4eab
Maria Kalinina <marykalinina86449@gmail.com>
2026-06-08 12:17:12 +0300

    Add files via upload

---
+++ 03.arrays/05-Ex_max.js
@@ -0,0 +1,26 @@
+// Task: Top Integers
+// Description: Find all numbers that are greater than every element to their right.
+
+function maxNumber(arr) {
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentNum = arr[i];
+        let isMaxNum = true;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] >= currentNum) {
+                isMaxNum = false;
+                break;
+            }
+        }
+        if (isMaxNum) {
+            result.push(currentNum);
+        }
+    }
+    console.log(result.join(' '));
+
+}
+maxNumber([1, 4, 3, 2]);
+maxNumber([14, 24, 3, 19, 15, 17]);
+maxNumber([41, 41, 34, 20]);
