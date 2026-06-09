d652c6782a04c01c79bcd0e3757593708e3e1653
Maria Kalinina <marykalinina86449@gmail.com>
2026-06-09 19:57:57 +0300

    Add files via upload

---
+++ 03.arrays/08-Ex_magic.js
@@ -0,0 +1,16 @@
+// Task: Magic Sum
+// Description: Find and print all pairs of numbers 
+// whose sum is equal to the given number.
+
+function magicSum(arr, targetSum) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === targetSum) {
+                console.log(arr[i], arr[j]);
+            }
+        }
+    }
+}
+magicSum([1, 7, 6, 2, 19, 23], 8);
+magicSum([14, 20, 60, 13, 7, 19, 8], 27);
+magicSum([1, 2, 3, 4, 5, 6], 6);
