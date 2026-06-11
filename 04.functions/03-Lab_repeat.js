02fe1bfa5b8a1bebcfffba057e8ebc22856f1645
Maria Kalinina <marykalinina86449@gmail.com>
2026-06-11 13:55:38 +0300

    Add files via upload

---
+++ 04.functions/03-Lab_repeat.js
@@ -0,0 +1,14 @@
+// Task: Repeat String
+// Description: Return a string repeated n times.
+
+function repeatString(str, count) {
+    let result = '';
+
+    for (let i = 0; i < count; i++) {
+        result += str;
+    }
+
+    return result;
+}
+console.log(repeatString("abc", 3));
+console.log(repeatString("String", 2));
