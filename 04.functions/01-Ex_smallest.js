bcce9a6f7245ec865d23bcbfe47b9a9fd396f4ad
Maria Kalinina <marykalinina86449@gmail.com>
2026-06-12 20:50:34 +0300

    Add files via upload

---
+++ 04.functions/01-Ex_smallest.js
@@ -0,0 +1,10 @@
+// Task: Smallest of Three Numbers.
+// Description: Receive three integers and print the smallest one.
+
+function findSmallestNum(num1, num2, num3) {
+    console.log(Math.min(num1, num2, num3));
+}
+
+findSmallestNum(2, 5, 3);
+findSmallestNum(600, 342, 123);
+findSmallestNum(2, 2, 2);
