7f9e33007ee79f6ac059efbd30fdfe6015c925ce
Maria Kalinina <marykalinina86449@gmail.com>
2026-07-13 18:46:46 +0300

    Create 01-Lab_print.js

---
+++ 08.text_processing/01-Lab_print.js
@@ -0,0 +1,11 @@
+// Task: Print Characters.
+// Description: Receive a string.
+// Print each character from the string.
+// Print every character on a separate line.
+
+function printCharacters(str) {
+    for (let letter of str) {
+        console.log(letter);
+    }
+}
+printCharacters('AWord');
