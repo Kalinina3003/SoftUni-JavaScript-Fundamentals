dcc182446357b40a5d42cfbdd9ed00d3796627df
Maria Kalinina <marykalinina86449@gmail.com>
2026-08-06 11:35:06 +0300

    Update 04-Exam_movie.js

--- 05.nested_loop/04-Exam_movie.js
+++ 05.nested_loop/04-Exam_movie.js
@@ -24,3 +24,28 @@ function movieTickets(a1, a2, n) {
 }
 movieTickets(86, 88, 4);
 movieTickets(71, 74, 6);
+
+/*
+function movieTickets(a1, a2, n) {
+    let sum = 0;
+
+    for (let first = a1; first < a2; first++) {
+        if (first % 2 === 0) {
+            continue;
+        }
+
+        let asciiFirst = String.fromCharCode(first);
+
+        for (let second = 1; second <= n - 1; second++) {
+            for (let third = 1; third <= (n / 2) - 1; third++) {
+
+                sum = second + third + first;
+
+                if (sum % 2 !== 0) {
+                    console.log(`${asciiFirst}-${second}${third}${first}`);
+                }
+            }
+        }
+    }
+}
+*/
