653d32e8f6774bb041196b88d1497f4292498fdb
Maria Kalinina <marykalinina86449@gmail.com>
2026-08-08 16:32:24 +0300

    Add files via upload

---
+++ 02.data_types/04-More_biggest.js
@@ -0,0 +1,12 @@
+// Task: Biggest of Three Numbers
+// Description: Find and print the biggest of three numbers.
+
+function biggestOfThreeNumbers(num1, num2, num3) {
+    let biggest = Math.max(num1, num2, num3);
+
+    console.log(biggest);
+}
+biggestOfThreeNumbers(-2, 7, 3);
+biggestOfThreeNumbers(130, 5, 99);
+biggestOfThreeNumbers(43, 43.2, 43.1);
+biggestOfThreeNumbers(2, 2, 2);
