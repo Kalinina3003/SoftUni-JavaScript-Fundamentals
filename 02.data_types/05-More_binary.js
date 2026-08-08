cdcd182e9b262b085c7dfb09137ba7de3db87081
Maria Kalinina <marykalinina86449@gmail.com>
2026-08-08 16:38:42 +0300

    Add files via upload

---
+++ 02.data_types/05-More_binary.js
@@ -0,0 +1,10 @@
+// Task: Binary to Decimal
+// Description: Convert an 8-bit binary number from a string to a decimal number.
+
+function binaryToDecimal(binary) {
+    let decimal = parseInt(binary, 2);
+
+    console.log(decimal);
+}
+binaryToDecimal('00001001');
+binaryToDecimal('11110000');
