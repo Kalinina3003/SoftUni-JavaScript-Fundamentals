af8d244e2f95a1edf0c4be5e517ea65af7309f25
Maria Kalinina <marykalinina86449@gmail.com>
2026-08-14 17:19:26 +0300

    Update 10-Ex_thePyramid.js

--- 02.data_types/10-Ex_thePyramid.js
+++ 02.data_types/10-Ex_thePyramid.js
@@ -40,3 +40,39 @@ thePyramidOfKingDjoser(11, 1);
 thePyramidOfKingDjoser(11, 0.75);
 thePyramidOfKingDjoser(12, 1);
 thePyramidOfKingDjoser(23, 0.5);
+
+/*
+function thePyramidOfKingDjoser(base, increment) {
+    let stone = 0;
+    let marble = 0;
+    let laisLazuli = 0;
+    let gold = 0;
+    let step = 1; 
+
+    while (base > 2) {
+        let stoneStage = (base - 2) * (base - 2); 
+        stone += stoneStage * increment;
+
+        if (step % 5 === 0) {   
+            let laisLazuliStage = (base * base) - stoneStage;
+            laisLazuli += laisLazuliStage * increment;
+        } else {
+            let marbleStage = (base * base) - stoneStage;
+            marble += marbleStage * increment;
+        }
+
+        base -= 2; 
+        step++;     
+    }
+
+    gold = base * base * increment; 
+
+    let height = step * increment;
+
+    console.log(`Stone required: ${Math.ceil(stone)}`);
+    console.log(`Marble required: ${Math.ceil(marble)}`);
+    console.log(`Lapis Lazuli required: ${Math.ceil(laisLazuli)}`);
+    console.log(`Gold required: ${Math.ceil(gold)}`);
+    console.log(`Final pyramid height: ${Math.floor(height)}`);
+}
+*/
