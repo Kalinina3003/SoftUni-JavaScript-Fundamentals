0c7fae97cb443e1577f5114cce674cec50e51218
Maria Kalinina <marykalinina86449@gmail.com>
2026-08-20 17:30:09 +0300

    Update 08-Ex_calculator.js

--- 02.data_types/08-Ex_calculator.js
+++ 02.data_types/08-Ex_calculator.js
@@ -18,3 +18,22 @@ function calculator(num1, operator, num2) {
 }
 calculator(5, '+', 10);
 calculator(25.5, '-', 3);
+
+/*
+function calculator(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            console.log((num1 + num2).toFixed(2));
+            break;
+        case '-':
+            console.log((num1 - num2).toFixed(2));
+            break;
+        case '/':
+            console.log((num1 / num2).toFixed(2));
+            break;
+        case '*':
+            console.log((num1 * num2).toFixed(2));
+            break;
+    }
+}
+*/
