d39108f6c3f506fc8b074d19116ca179ce908b81
Maria Kalinina <marykalinina86449@gmail.com>
2026-08-21 17:35:58 +0300

    Update 04-More_biggest.js

--- 02.data_types/04-More_biggest.js
+++ 02.data_types/04-More_biggest.js
@@ -10,3 +10,23 @@ biggestOfThreeNumbers(-2, 7, 3);
 biggestOfThreeNumbers(130, 5, 99);
 biggestOfThreeNumbers(43, 43.2, 43.1);
 biggestOfThreeNumbers(2, 2, 2);
+
+/*
+function biggestOf3Numbers(num1, num2, num3) {
+    let biggest = Number.MIN_SAFE_INTEGER;
+
+    if (num1 > biggest) {
+        biggest = num1;
+    }
+
+    if (num2 > biggest) {             
+        biggest = num2;
+    }
+
+    if (num3 > biggest) {
+        biggest = num3;
+    }
+
+    console.log(biggest);
+}
+*/
