e4e16b8d6ff66edce44d66e68b092235c7228347
Maria Kalinina <marykalinina86449@gmail.com>
2026-08-25 20:55:26 +0300

    Update 03-Lab_reverse.js

--- 03.arrays/03-Lab_reverse.js
+++ 03.arrays/03-Lab_reverse.js
@@ -20,3 +20,17 @@ function reverseAnArrayOfNumbers(num, inputArr) {
 reverseAnArrayOfNumbers(3, [10, 20, 30, 40, 50]);
 reverseAnArrayOfNumbers(4, [-1, 20, 99, 5]);
 reverseAnArrayOfNumbers(2, [66, 43, 75, 89, 47]);
+
+/*
+function reverseInPlace(n, arr) {
+    let newArr = [];
+
+    for (let i = 0; i < n; i++) {
+        newArr.push(arr[i]);
+    }
+
+    let data = newArr.reverse();
+
+    console.log(data.join(' '));
+}
+*/
