8d4f270be6e6831853237f48868cc8f467a68bdf
Maria Kalinina <marykalinina86449@gmail.com>
2026-08-25 21:43:27 +0300

    Update 04-Lab_reverseIn.js

--- 03.arrays/04-Lab_reverseIn.js
+++ 03.arrays/04-Lab_reverseIn.js
@@ -18,3 +18,11 @@ function reverseInPlace(arr) {
 reverseInPlace(['a', 'b', 'c', 'd', 'e']);
 reverseInPlace(['abc', 'def', 'hig', 'klm', 'nop']);
 reverseInPlace(['33', '123', '0', 'dd']);
+
+/*
+function reverseInPlace(arr) {
+    arr.reverse();
+
+    console.log(arr.join(' '));
+}
+*/
