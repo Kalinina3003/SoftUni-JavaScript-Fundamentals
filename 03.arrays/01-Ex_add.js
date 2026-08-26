ca55cdeeb290d42d02e45737016b3272641c7458
Maria Kalinina <marykalinina86449@gmail.com>
2026-08-26 18:33:27 +0300

    Update 01-Ex_add.js

--- 03.arrays/01-Ex_add.js
+++ 03.arrays/01-Ex_add.js
@@ -26,3 +26,26 @@ function addAndSubtract(arr) {
 }
 addAndSubtract([5, 15, 23, 56, 35]);
 addAndSubtract([-5, 11, 3, 0, 2]);
+
+/*
+function addAndSubtract(arr) {
+    let newArr = [];
+    let sumArr = 0;
+    let sumNewArr = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        sumArr += num;
+
+        num % 2 === 0 ? num += i : num -= i;
+     
+        sumNewArr += num;
+
+        newArr.push(num);
+    }
+
+    console.log(newArr);
+    console.log(sumArr);
+    console.log(sumNewArr);
+}
+*/
