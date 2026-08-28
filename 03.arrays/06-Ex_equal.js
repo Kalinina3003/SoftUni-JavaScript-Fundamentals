c494680beb71f61032ddcb6fa3eec5fcbe8b8825
Maria Kalinina <marykalinina86449@gmail.com>
2026-08-28 16:14:48 +0300

    Update 06-Ex_equal.js

--- 03.arrays/06-Ex_equal.js
+++ 03.arrays/06-Ex_equal.js
@@ -25,3 +25,29 @@ function equalSums(arr) {
 equalSums([1, 2, 3, 3]);
 equalSums([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4]);
 equalSums([1, 2, 3]);
+
+/*
+function equalSums(arr) {
+    let sumLeft = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let left = arr[i];
+
+        let sumRight = 0;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            let right = arr[j];
+            sumRight += right;
+        }
+
+        if (sumLeft === sumRight) {
+            console.log(i);
+            return;
+        }
+
+        sumLeft += left;
+    }
+
+    console.log('no');
+}
+*/
