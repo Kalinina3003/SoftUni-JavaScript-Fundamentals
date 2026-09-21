f36dbc16ef37b1f44e741186163f542f34f4a197
Maria Kalinina <marykalinina86449@gmail.com>
2026-09-21 16:57:28 +0300

    Update 10-Ex_ladybugs.js

--- 03.arrays/10-Ex_ladybugs.js
+++ 03.arrays/10-Ex_ladybugs.js
@@ -69,5 +69,72 @@ function ladybugs(input) {
     console.log(field.join(' '));
 }
 ladybugs([3, '0 1', '0 right 1', '2 right 1']);
+// 0 1 0;
+
 ladybugs([3, '0 1 2', '0 right 1', '1 right 1', '2 right 1']);
+// 0 0 0;
+
 ladybugs([5, '3', '3 left 2', '1 left -2']);
+// 0 0 0 1 0;
+
+/*
+function ladybugs(arr) {
+    let sizeOfField = arr[0];  /
+    let occupiedCells = arr[1].split(' ').map(Number);
+    let field = [];
+
+    for (let i = 0; i < sizeOfField; i++) {
+        field.push(0);
+    }
+
+    for (let ladybugsPosition of occupiedCells) {
+        if (ladybugsPosition >= 0 && ladybugsPosition < sizeOfField) {
+            field[ladybugsPosition] = 1;  
+        }
+    }
+
+    for (let i = 2; i < arr.length; i++) {
+        let command = arr[i].split(' ');
+
+        let startOfMovement = Number(command[0]);
+        let direction = command[1];
+        let flyLength = Number(command[2]);
+
+        if (startOfMovement < 0 || startOfMovement >= sizeOfField) {
+            continue;
+        }
+
+        if (field[startOfMovement] === 0) {
+            continue;
+        }
+
+        if (flyLength === 0) {
+            continue;
+        }
+
+        let step;
+
+        if (direction === 'right') {
+            step = flyLength;       
+        } else if (direction === 'left') {
+            step = -flyLength;
+        }
+
+        let newPosition = startOfMovement + step;
+
+        while (newPosition >= 0 && newPosition < sizeOfField && field[newPosition] === 1) {
+            newPosition += step;
+        }
+
+        if (newPosition < 0 || newPosition >= sizeOfField) {
+            field[startOfMovement] = 0;
+            continue;
+        }
+
+        field[startOfMovement] = 0;
+        field[newPosition] = 1;
+    }
+
+    console.log(field.join(' '));
+}
+*/
