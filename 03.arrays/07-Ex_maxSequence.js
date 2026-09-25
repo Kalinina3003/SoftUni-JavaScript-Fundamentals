891ff376b67a6c212c88998d222b1e7692187054
Maria Kalinina <marykalinina86449@gmail.com>
2026-09-25 15:47:05 +0300

    Update 07-Ex_maxSequence.js

--- 03.arrays/07-Ex_maxSequence.js
+++ 03.arrays/07-Ex_maxSequence.js
@@ -22,8 +22,13 @@ function maxSequenceOfEqual(arr) {
     console.log((bestNumber + ' ').repeat(bestLength).trim());
 }
 maxSequenceOfEqual([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
+// 2 2 2;
+
 maxSequenceOfEqual([1, 1, 1, 2, 3, 1, 3, 3]);
+// 1 1 1;
+
 maxSequenceOfEqual([0, 1, 1, 5, 2, 2, 6, 3, 3]);
+// 1 1;
 
 /*
 function maxSequence(arr) {
@@ -46,4 +51,30 @@ function maxSequence(arr) {
     }
 
     console.log(longest.join(' '));
+*/
+
+/*
+function solve(arr) {
+    let count = 1;
+    let maxCount = 0;
+    let mainNum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentNum = arr[i];
+        let nextNum = arr[i + 1];
+
+        if (count > maxCount) {
+            maxCount = count;
+            mainNum = currentNum;
+        }
+
+        if (currentNum === nextNum) {
+            count++;
+        } else {
+            count = 1;
+        }
+    }
+
+    console.log(Array(maxCount).fill(mainNum).join(' '));
+}
 */
